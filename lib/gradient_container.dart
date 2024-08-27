b2ff7de467e9d9c1c2aca6d5dc319973de71ac1a
Wendy Private <lienyun.chang@gmail.com>
2024-08-27 16:58:34 +0800

    41. Alignment?

--- lib/gradient_container.dart
+++ lib/gradient_container.dart
@@ -1,21 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:first_app/white_text.dart';
 
-var startAlignment = Alignment.topLeft; // var沒有被鎖定
+Alignment? startAlignment; // null
 var endAlignment = Alignment.bottomRight;
 
 class GradientContainer extends StatelessWidget {
-  // GradientContainer({ key }): super(key: key); 簡寫如下：
   const GradientContainer({super.key});
 
   @override
-  // 函數前面的type是返回值type
   Widget build(context) {
-    // return 到UI上
+    startAlignment = Alignment.topLeft;
     return Container(
       decoration: BoxDecoration(
         gradient: LinearGradient(
-          // const 鎖定顏色
           colors: const [
             Color.fromARGB(255, 243, 120, 161),
             Color.fromARGB(255, 104, 198, 200),
