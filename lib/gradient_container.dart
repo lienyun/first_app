c003a57e9865af9bd95aa4efb478a28ee81ba113
Wendy Private <lienyun.chang@gmail.com>
2024-08-27 15:56:58 +0800

    custom widget file

---
+++ lib/gradient_container.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+class GradientContainer extends StatelessWidget {
+  // GradientContainer({ key }): super(key: key); 簡寫如下：
+  const GradientContainer({super.key});
+
+  @override
+  // 函數前面的type是返回值type
+  Widget build(context) {
+    // return 到UI上
+    return Container(
+      decoration: const BoxDecoration(
+        gradient: LinearGradient(
+          colors: [
+            Color.fromARGB(255, 243, 120, 161),
+            Color.fromARGB(255, 104, 198, 200),
+          ],
+          begin: Alignment.topLeft,
+          end: Alignment.bottomRight,
+        ),
+      ),
+      child: const Center(
+        child: Text(
+          'Hello World!',
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 28,
+          ),
+        ),
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'package:first_app/gradient_container.dart';
+
 void main() {
   runApp(
     const MaterialApp(
@@ -8,37 +10,4 @@ void main() {
       ),
     ),
   );
-}
-
-// 因為是個widget所以要加上extends
-class GradientContainer extends StatelessWidget {
-  // GradientContainer({ key }): super(key: key); 簡寫如下：
-  const GradientContainer({super.key});
-
-  @override
-  // 函數前面的type是返回值type
-  Widget build(context) {
-    // return 到UI上
-    return Container(
-      decoration: const BoxDecoration(
-        gradient: LinearGradient(
-          colors: [
-            Color.fromARGB(255, 243, 120, 161),
-            Color.fromARGB(255, 104, 198, 200),
-          ],
-          begin: Alignment.topLeft,
-          end: Alignment.bottomRight,
-        ),
-      ),
-      child: const Center(
-        child: Text(
-          'Hello World!',
-          style: TextStyle(
-            color: Colors.white,
-            fontSize: 28,
-          ),
-        ),
-      ),
-    );
-  }
 }
